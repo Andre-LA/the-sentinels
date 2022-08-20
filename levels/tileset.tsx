89467c97cb67cac48981a3df9e5626bd96e4c250
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2022-08-20 12:27:35 -0300

    game is almost finished (sorry for the messy diff)

---
+++ levels/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="tileset" tilewidth="8" tileheight="8" tilecount="208" columns="16">
+ <image source="tileset.png" width="128" height="104"/>
+</tileset>
